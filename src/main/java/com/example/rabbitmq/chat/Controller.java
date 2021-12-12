c94bc20afa44d612adf8af34186c43ecc2b2b048
kevenallan <kevenallan98@gmail.com>
2021-12-12 17:23:12 -0300

    ..

--- src/main/java/com/example/rabbitmq/chat/Controller.java
+++ src/main/java/com/example/rabbitmq/chat/Controller.java
@@ -3,6 +3,7 @@ package com.example.rabbitmq.chat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
 
 	private List<String> msgs = new ArrayList<>();
